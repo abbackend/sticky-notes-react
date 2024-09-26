e53f22f5079465feae175860be6c1b3a27a164bb
Ashish Bairagi <abbackend98@gmail.com>
2024-09-26 11:06:09 +0530

    Add files via upload

---
+++ app/icons/Check.jsx
@@ -0,0 +1,15 @@
+const Check = ({ size = 20 }) => {
+    return (
+        <svg
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            height={size}
+            width={size}
+        >
+            <path d="m2.67 7.63 2.79 2.78 7.87-7.87 1.52 1.52-9.39 9.4-4.31-4.31 1.52-1.52z" />
+        </svg>
+    )
+}
+
+export default Check
---
+++ app/icons/Cross.jsx
@@ -0,0 +1,15 @@
+const Cross = ({ size = 24 }) => {
+    return (
+        <svg
+            fill="currentColor"
+            height={size}
+            width={size}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 490 490"
+        >
+            <g strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <polygon points="386.813,0 245,141.812 103.188,0 0,103.188 141.813,245 0,386.812 103.187,489.999 245,348.187 386.813,490 490,386.812 348.187,244.999 490,103.187 "></polygon> </g>
+        </svg>
+    )
+}
+
+export default Cross
